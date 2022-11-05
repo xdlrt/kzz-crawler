93f88401e60031c3bcceea7c32d06b8665671add
yeshu <673643706@qq.com>
2022-11-05 16:27:14 +0800

    init this project

---
+++ tests/kzz.spec.ts
@@ -0,0 +1,60 @@
+import { test, expect } from '@playwright/test';
+import { BitableRecords, execute, JisiluRecord } from '../utils/jisilu';
+
+test('test', async ({ page, request }) => {
+
+  console.log('手机号', process.env.phone);
+  console.log('密码', process.env.secret);
+
+  await page.goto('https://www.jisilu.cn/data/cbnew/#cb');
+
+  await page.getByRole('button', { name: '登录' }).click();
+  await expect(page).toHaveURL('https://www.jisilu.cn/account/login/');
+
+  await page.getByPlaceholder('手机号/用户名').click();
+
+  await page.getByPlaceholder('手机号/用户名').fill(process.env.phone || '');
+
+  await page.getByPlaceholder('密码').click();
+
+  await page.getByPlaceholder('密码').fill(process.env.secret || '');
+
+  await page.locator('form:has-text("帐号密码登录 忘记密码 记住我 本人已阅读并同意《用户协议》和《隐私政策》 登录") input[type="checkbox"]').nth(1).check();
+
+  await page.getByRole('button', { name: '登录' }).click();
+
+  page.on('response', (response) => {
+    if (response.url().includes('/data/cbnew/cb_list_new')) {
+      response.json().then((res) => {
+        const records: BitableRecords = [];
+
+        res.rows.forEach((row) => {
+          const rowArr = Object.values(row);
+          const rowData = rowArr[1] as JisiluRecord;
+          records.push({
+            fields: {
+              '代码': rowData.bond_id,
+              '转债名称': rowData.bond_nm,
+              '现价': rowData.price,
+              '转股溢价率': rowData.premium_rt / 100,
+              '双低': rowData.dblow,
+            }
+          });
+        });
+
+        console.log('app_token', process.env.app_token);
+        console.log('table_id', process.env.table_id);
+
+        execute(request, {
+          app_token: process.env.app_token || '',
+          table_id: process.env!.table_id || '',
+          records,
+        });
+      });
+    }
+  });
+
+  await expect(page).toHaveURL('https://www.jisilu.cn/data/cbnew/#cb');
+
+  await page.waitForTimeout(5000);
+});
---
+++ utils/jisilu/index.ts
@@ -0,0 +1,156 @@
+import { APIRequestContext } from '@playwright/test';
+
+// 集思录数据行结构
+export interface JisiluRecord {
+  bond_id: string;
+  bond_nm: string;
+  price: number;
+  premium_rt: number;
+  dblow: number;
+}
+
+// 多维表格行结构
+export interface BitableRecord {
+  '代码': string;
+  '转债名称': string;
+  '现价': number;
+  '转股溢价率': number;
+  '双低': number;
+}
+
+export type BitableRecords = Array<{
+  fields: BitableRecord;
+  record_id?: string;
+}>;
+
+export async function fetchTenantAccessToken(request: APIRequestContext) {
+  const url = 'https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal';
+  console.log('app_id', process.env.app_id);
+  console.log('app_secret', process.env.app_secret);
+  const response = await request.post(url, {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    data: {
+      "app_id": process.env.app_id,
+      "app_secret": process.env.app_secret
+    }
+  });
+  const json = await response.json();
+  return json;
+}
+
+interface ICommonParams {
+  token: string;
+  app_token: string;
+  table_id: string;
+}
+
+export interface IBatchCreateParams extends ICommonParams {
+  records: BitableRecords;
+}
+
+export async function batchCreate(request: APIRequestContext, params: IBatchCreateParams) {
+  const { token, app_token, table_id, records } = params;
+  const response = await request.post(
+    `https://open.feishu.cn/open-apis/bitable/v1/apps/${app_token}/tables/${table_id}/records/batch_create`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      data: {
+        records,
+      },
+    });
+  const json = await response.json();
+  return json;
+}
+
+export async function batchUpdate(request: APIRequestContext, params: IBatchCreateParams) {
+  const { token, app_token, table_id, records } = params;
+  const response = await request.post(
+    `https://open.feishu.cn/open-apis/bitable/v1/apps/${app_token}/tables/${table_id}/records/batch_update`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      data: {
+        records,
+      },
+    });
+  const json = await response.json();
+  return json;
+}
+
+export interface IBatchQueryParams extends ICommonParams {
+  pageSize?: number; // 上限 500
+}
+
+export async function batchQuery(request: APIRequestContext, params: IBatchQueryParams) {
+  const { token, app_token, table_id, pageSize = 500 } = params;
+  const response = await request.get(
+    `https://open.feishu.cn/open-apis/bitable/v1/apps/${app_token}/tables/${table_id}/records`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    data: {
+      pageSize,
+    }
+  });
+  const json = await response.json();
+  return json;
+}
+
+// originRecords 表中原有数据
+// targetRecords 新爬取的数据
+export function makeRecords(
+  originRecords: BitableRecords,
+  targetRecords: BitableRecords) {
+  const createRecords: BitableRecords = [];
+  const updateRecords: BitableRecords = [];
+  targetRecords.forEach(target => {
+    const origin = originRecords.find(origin => origin.fields['代码'] === target.fields['代码']);
+    if (origin) {
+      updateRecords.push({ ...target, record_id: origin.record_id });
+    } else {
+      createRecords.push(target);
+    }
+  });
+  return { createRecords, updateRecords };
+}
+
+export async function execute(request: APIRequestContext, params: Omit<IBatchCreateParams, 'token'>) {
+  const { tenant_access_token } = await fetchTenantAccessToken(request);
+
+  const commonOptions = {
+    token: tenant_access_token,
+    app_token: params.app_token,
+    table_id: params.table_id,
+  };
+
+  const targetRecords = params.records;
+  const { data: { items: originRecords } } = await batchQuery(request, commonOptions);
+
+  const { createRecords, updateRecords } = makeRecords(originRecords, targetRecords);
+
+  const createOptions = {
+    ...commonOptions,
+    records: createRecords,
+  };
+  const createResponse = await batchCreate(request, createOptions);
+
+  console.log('createResponse', createResponse);
+
+  const updateOptions = {
+    ...commonOptions,
+    records: updateRecords,
+  };
+  const updateResponse = await batchUpdate(request, updateOptions);
+
+  console.log('updateResponse', updateResponse);
+
+  return { createResponse, updateResponse };
+}
